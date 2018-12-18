40720875a6371b4dd5d1a3953f2531cae9215da1
TBBOES01 <thomas.boes@stu.jefferson.kyschools.us>
2018-12-18 07:38:53 -0500

    2nd Commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
 import matplotlib
+from matplotlib import pyplot as plt
+
 
 filename = 'urban-heat-management-actions-neighborhood-data-1.csv'
 with open(filename) as f:
@@ -8,7 +10,18 @@ with open(filename) as f:
     print(header_row)
 
     new_trees = []
+    neighborhoods = []
     for row in reader:
-        new_trees.append(row[3])
+        neighborhoods.append(str(row[0]))
+        new_trees.append(int(row[3]))
 
     print(new_trees)
+    print(neighborhoods)
+
+    objects = neighborhoods
+    plt.bar((len(objects)), new_trees, align='center', alpha=0.5)
+    plt.xticks(objects)
+    plt.title("New trees planted", fontsize=24)
+    plt.xlabel(objects, fontsize=16)
+    plt.ylabel("Number of trees", fontsize=16)
+    plt.show()
